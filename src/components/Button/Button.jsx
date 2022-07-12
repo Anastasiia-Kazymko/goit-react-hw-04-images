830712b241ff691ea1942c54334a479686fd7b61
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2022-07-12 13:46:11 +0300

    add settings

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+/* export const Filter = ({ onFilter, value }) => {
+  return (
+    <>
+      <p>Find contacts by name</p>
+      <input type="text" name="filter" value={value} onChange={onFilter} />
+    </>
+  );
+};
+ */
